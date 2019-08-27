1326a71e75e4405eaf6c25e4a59e763033087f26
Miguel Nunes <mnunes@redhat.com>
2019-08-26 23:36:38 -0300

    fixing oc new-build

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -22,7 +22,7 @@ oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\
       USER 1001' --name=jenkins-agent-appdev -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
-oc new-build --name=tasks https://github.com/micaelis72/advdevtraining.git --contextDir=openshift-tasks -n ${GUID}-jenkins
+oc new-build --name=tasks https://github.com/micaelis72/advdevtraining.git --context-dir=openshift-tasks -n ${GUID}-jenkins
 
 # Make sure that Jenkins is fully up and running before proceeding!
 while : ; do
